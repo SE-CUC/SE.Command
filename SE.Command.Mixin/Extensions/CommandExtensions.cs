ca252925f0acb552a9d5812a3abb3a867923c664
k-edr <yevhenpuzenko@gmail.com>
2025-08-19 00:06:52 +0200

    Integrated Command module with SE.SEApplication

---
+++ SE.Command.Mixin/Extensions/CommandExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace IngameScript
+{
+    public static class CommandExtensions
+    {
+        public static SEApplicationBuilder AddCommands(this SEApplicationBuilder builder)
+        {
+        
+            builder.Services.AddSingleton<ICommandService>(sp => new CommandService(sp.GetService<ILogger>()));
+           
+            builder.OnTerminalAction((arg, update, sp) =>
+            {
+                if (!string.IsNullOrWhiteSpace(arg))
+                {
+                    var commandService = sp.GetService<ICommandService>();
+                    commandService.Execute(arg);
+                }
+            });
+
+            return builder;
+        }
+    }
+}
