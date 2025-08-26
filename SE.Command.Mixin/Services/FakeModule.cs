dd7ea4bb265e8f9361f578efc522888c9fd83486
k-edr <yevhenpuzenko@gmail.com>
2025-08-26 16:05:33 +0200

    added extension for SE.SEApplication.

---
+++ SE.Command.Mixin/Services/FakeModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IngameScript
+{
+    public class FakeModule : ICommandModule
+    {
+        private readonly IEnumerable<ICommand> _commands;
+
+        public FakeModule(IEnumerable<ICommand> commands)
+        {
+            _commands = commands;
+        }
+
+        public IEnumerable<ICommand> GetCommands() => _commands;
+    }
+}
---
+++ SE.SEApplication.CommandExtension.Mixin/CommandExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IngameScript
+{
+    public static class CommandExtensions
+    {
+        public static SEApplicationBuilder AddCommands(
+            this SEApplicationBuilder builder,
+            IEnumerable<ICommandModule> modules = null,
+            IEnumerable<ICommand> commands = null)
+        {
+            builder.Services.AddSingleton<ICommandService>(sp => 
+            {
+                var cs = new CommandService(sp.GetService<ILogger>());
+
+                if (modules != null) 
+                {
+                    foreach (var module in modules)
+                    {
+                        cs.RegisterModule(module);
+                    }
+                }
+                if(commands != null)
+                {
+                    cs.RegisterModule(new FakeModule(commands));
+                }
+
+                return cs;
+            });
+
+            builder.OnTerminalAction((arg, update, sp) =>
+            {
+                if (!string.IsNullOrWhiteSpace(arg))
+                {
+                    var commandService = sp.GetService<ICommandService>();
+                    commandService.Execute(arg);
+                }
+            });
+
+            return builder;
+        }  
+        
+        public static SEApplicationBuilder AddCommand(this SEApplicationBuilder builder, Func<SEApplicationBuilder, SEApplicationBuilder> func) => func(builder);
+    }
+}
